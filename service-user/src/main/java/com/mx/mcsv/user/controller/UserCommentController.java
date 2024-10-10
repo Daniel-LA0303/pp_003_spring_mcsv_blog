5f452463f64bc37e617f61de8edadd0a000ed7ce
daniel <03daniel030399@gmail.com>
2024-10-10 01:10:51 -0600

    add circuit breaker in openfeign client

--- service-user/src/main/java/com/mx/mcsv/user/controller/UserCommentController.java
+++ service-user/src/main/java/com/mx/mcsv/user/controller/UserCommentController.java
@@ -17,6 +17,8 @@ import com.mx.mcsv.user.exceptions.UserException;
 import com.mx.mcsv.user.service.UserService;
 import com.mx.mcsv.user.service.impl.UserCommentServiceImpl;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("/api/users/comment")
 public class UserCommentController {
@@ -28,6 +30,7 @@ public class UserCommentController {
 	private UserService userService;
 
 	@PostMapping
+	@CircuitBreaker(name = "commentsCB", fallbackMethod = "fallBackCreateComment")
 	public ResponseEntity<?> createComment(@RequestBody CommentRequestDTO commentRequestDTO) throws UserException {
 		userService.findById(commentRequestDTO.getUserId());
 
@@ -37,6 +40,7 @@ public class UserCommentController {
 	}
 
 	@DeleteMapping("/delete-comment/{id}/{userId}/{blogId}")
+	@CircuitBreaker(name = "commentsCB", fallbackMethod = "fallBackDeleteComment")
 	public ResponseEntity<?> deteleteComment(@PathVariable Long id, @PathVariable Long userId,
 			@PathVariable Long blogId) throws UserException {
 
@@ -49,6 +53,7 @@ public class UserCommentController {
 	}
 
 	@GetMapping("/by-blog/{id}")
+	@CircuitBreaker(name = "commentsCB", fallbackMethod = "fallBackGetCommentsByBlog")
 	public ResponseEntity<?> getCommentsByBlog(@PathVariable Long id) throws UserException {
 		ApiResponse<Object, Object> apiResponse = userCommentService.getCommentsByBlog(id);
 
@@ -56,12 +61,29 @@ public class UserCommentController {
 	}
 
 	@GetMapping("/by-user/{id}")
+	@CircuitBreaker(name = "commentsCB", fallbackMethod = "fallBackGetCommentsByUser")
 	public ResponseEntity<?> getCommentsByUser(@PathVariable Long id) throws UserException {
 		userService.findById(id);
 
 		ApiResponse<Object, Object> apiResponse = userCommentService.getCommentsByUser(id);
 
 		return new ResponseEntity<>(apiResponse, HttpStatus.valueOf(apiResponse.getStatus()));
+	}
+
+	private ResponseEntity<?> fallBackCreateComment(CommentRequestDTO commentRequestDTO, RuntimeException e) {
+		return new ResponseEntity<>("Unable to create comment at the moment, please try again later.", HttpStatus.OK);
+	}
+
+	private ResponseEntity<?> fallBackDeleteComment(Long id, Long userId, Long blogId, RuntimeException e) {
+		return new ResponseEntity<>("Unable to delete comment at the moment.", HttpStatus.OK);
+	}
+
+	private ResponseEntity<?> fallBackGetCommentsByBlog(Long id, RuntimeException e) {
+		return new ResponseEntity<>("Unable to retrieve comments for this blog at the moment.", HttpStatus.OK);
+	}
+
+	private ResponseEntity<?> fallBackGetCommentsByUser(Long id, RuntimeException e) {
+		return new ResponseEntity<>("Unable to retrieve comments for this user at the moment.", HttpStatus.OK);
 	}
 
 }
